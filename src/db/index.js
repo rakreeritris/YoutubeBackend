fcfc46fd05ac70a2c1aef79a9ab73974cc898514
Ritik Gupta <rakreeritris@gmail.com>
2025-04-20 19:00:18 +0530

    creating utils

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+export const connectDB=async()=>{
+    try {
+    const connectionInstance=   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MONGODB connected !! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        
+        console.log('MONGODB CONNECTION ERROR',error);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import {connectDB} from './db/index.js'
+import { app } from './app.js';
+dotenv.config();
+connectDB()
+.then(
+   app.listen(process.env.PORT,()=>{
+    console.log(`server is listening at port ${process.env.PORT}`);
+   }) 
+)
+.catch((err)=>{
+    console.log('MONGO db connection failed');
+})
