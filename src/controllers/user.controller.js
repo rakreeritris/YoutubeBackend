6d515480dd18ff19229a7e5e137d84d8fa431e00
Ritik Gupta <rakreeritris@gmail.com>
2025-04-27 13:35:51 +0530

    generate refrsh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {ApiError} from '../utils/ApiError.js';
 import { User } from '../models/user.model.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
+import jwt from 'jsonwebtoken';
 const generateAccessAndRefreshToken=async(userId)=>{
     try {
         const user=await User.findById(userId);
@@ -63,7 +64,6 @@ if (coverImageLocalPath) {
       res.status(200).json(new ApiResponse(201,createdUser,"user created successfully"));
     }
 )
-
 const loginUser=asyncHandler(async(req,res)=>{
     const {userName,email,password}=req.body;
 
@@ -108,8 +108,11 @@ const loginUser=asyncHandler(async(req,res)=>{
    },"User logged in successfully"));
 
 });
-
 const logoutUser=asyncHandler(async(req,res)=>{
+    const options={
+        httpOnly:true,
+        secure:true
+       }
     await User.findByIdAndUpdate(
         req.user._id,
         { $set: { refreshToken: undefined } },
@@ -118,8 +121,39 @@ const logoutUser=asyncHandler(async(req,res)=>{
 
       return res
       .status(200)
-      .clearCookie("accessToken",accessToken,options)
-      .clearCookie("refreshToken",refreshToken,options)
+      .clearCookie("accessToken",options)
+      .clearCookie("refreshToken",options)
       .json(new ApiResponse(200,{},"User logged out successfully"));
 });
-export {registerUser,loginUser,logoutUser};
+
+const refreshAccessToken=asyncHandler(async(req,res)=>{
+    const incomingRefreshToken=req.cookies.refreshToken||req.body.refreshToken;
+    if(!incomingRefreshToken)
+    {
+        throw new ApiError(401,"unauthorized request");
+    }
+       const decodedToken=jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET);
+       const user =await User.findById(decodedToken._id);
+       if(!user)
+       {
+        throw new ApiError(401,'Invalid refresh token');
+       }
+       if(incomingRefreshToken!==user?.refreshToken)
+       {
+        throw new ApiError(401,'Refresh token is expired or used')
+       }
+       const options={
+        httpOnly:true,
+        secure:true,
+       }
+       const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id);
+       return res
+   .status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("refreshToken",refreshToken,options)
+   .json(new ApiResponse(200,{
+    accessToken,
+    refreshToken
+   },"Access Token refreshed"));
+})
+export {registerUser,loginUser,logoutUser,refreshAccessToken};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser } from "../controllers/user.controller.js";
+import { registerUser,loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router=Router();
@@ -16,6 +16,7 @@ router.route('/register').post(upload.fields([
 ]) ,registerUser);
 //sercure routes
 router.route('/login').post(loginUser);
+router.route('/refresh').post(refreshAccessToken);
 router.route('/logout').post(verifyJwt,logoutUser)
 
 export default router;
