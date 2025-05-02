3348450dcfb7d21ccbdcfaffa9ed0114d215ebbf
Ritik Gupta <rakreeritris@gmail.com>
2025-05-02 21:14:04 +0530

    channel history and watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 const generateAccessAndRefreshToken=async(userId)=>{
     try {
         const user=await User.findById(userId);
@@ -126,6 +127,71 @@ const logoutUser=asyncHandler(async(req,res)=>{
       .json(new ApiResponse(200,{},"User logged out successfully"));
 });
 
+const changePassword=asyncHandler(async(req,res)=>{
+   const {oldPassword,newPassword}=req.body;
+
+   if(!oldPassword||!newPassword)
+   {
+    throw new ApiError(400,"All fields are required");
+   }
+   const user=await User.findById(req?.user?._id);
+
+   const isPasswordMatch=await user.isPasswordCorrect(oldPassword);
+   if(!isPasswordMatch)
+   {
+    throw new ApiError(401,'Old password is incorrect');
+   }
+   user.password=newPassword;
+   await user.save({validateBeforeSave:false})
+   res.status(200).json(new ApiResponse(200,'Password changed successfully'));
+}); 
+
+const getCurrentUser=asyncHandler(async(req,res)=>{
+    res
+    .status(200)
+    .json(new ApiResponse(200,req.user,"Current user fetched successfully"))
+});
+
+const updateAccountDetails=asyncHandler(async(req,res)=>{
+    const {fullName,email}=req.body;
+    if(!fullName||!email)
+    {
+        throw new ApiError(400,"All fields are required")
+    }
+    const user=await User.findByIdAndUpdate(req.user._id,{
+        $set:{
+            fullName,
+            email,
+        },
+    },{new:true}).select('-password');
+    res
+    .status(200)
+    .json(new ApiResponse(200,user,'User details updated successfully'));
+});
+
+const updateUserAvatar=asyncHandler(async(req,res)=>{
+    const avatarLocalPath=req.files?.avatar?.[0].path;
+    if(!avatarLocalPath)
+    {
+        throw new ApiError(400,"Avatar file is missing");
+    };
+
+    const avatar=await uploadOnCloudinary(avatarLocalPath);
+    if(!avatar.url)
+    {
+        throw new ApiError(400,'Error while uploading the file');
+    }
+
+    await User.findByIdAndUpdate(req.user._id,{
+        $set:{
+            avatar:avatar.url
+        }
+    }).select("-password");
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,{},'Avatar of user updated successfully'));
+})
 const refreshAccessToken=asyncHandler(async(req,res)=>{
     const incomingRefreshToken=req.cookies.refreshToken||req.body.refreshToken;
     if(!incomingRefreshToken)
@@ -156,4 +222,127 @@ const refreshAccessToken=asyncHandler(async(req,res)=>{
     refreshToken
    },"Access Token refreshed"));
 })
-export {registerUser,loginUser,logoutUser,refreshAccessToken};
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+    const {userName}=req.params;
+    if(!userName?.trim())
+    {
+        throw new ApiError(400,"userName is not present");
+    }
+   const channel=await  User.aggregate([{
+      $match:{
+        userName:userName
+      }
+    },{
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"channel",
+            as:"subscribers"
+        }
+    },{
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"subscriber",
+            as:"subscribedTo"
+        }
+    },{
+        $addFields:{
+            subscribersCount:{
+                $size:"$subscribers"
+            },
+            channelsSubscribedTo:{
+                $size:"$subscribedTo"
+            },
+            isSubscribed:{
+                $cond:{
+                    if:{$in:[req.user?._id,"$subscribers"]},
+                    then:true,
+                    else:false
+                }
+            }
+        }
+    },{
+        $project:{
+            fullName:1,
+            userName:1,
+            subscribersCount:1,
+            channelsSubscribedTo:1,
+            isSubscribed:1
+        }
+    }]);
+
+    if(!channel?.length)
+    {
+        throw new ApiError(404,"Channel does not exist");
+
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channel[0],"user channels details fetched successfully")
+    )
+});
+
+const getWatchHistory=asyncHandler(async(req,res)=>{
+    const user=await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        userName:1,
+                                        avatar:1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+    res.status(200)
+    .json(new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watchHistory fetched successfully"
+    ))
+})
+export {registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    getUserChannelProfile,
+    getWatchHistory
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser,loginUser, logoutUser, refreshAccessToken, updateAccountDetails, updateUserAvatar, changePassword, getCurrentUser, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 const router=Router();
@@ -14,9 +14,21 @@ router.route('/register').post(upload.fields([
         maxCount:1,
     }
 ]) ,registerUser);
-//sercure routes
+
 router.route('/login').post(loginUser);
 router.route('/refresh').post(refreshAccessToken);
+//sercure routes
+router.route('/updateDetails').post(verifyJwt,updateAccountDetails);
+router.route('/updateAvatar').post(verifyJwt,upload.fields([
+    {
+        name:'avatar',
+        maxCount:1,
+    }
+]),updateUserAvatar);
+router.route('/change-password').post(verifyJwt,changePassword);
+router.route('/get-user-details').post(verifyJwt,getCurrentUser);
+router.route('/channel/:userName').post(verifyJwt,getUserChannelProfile);
+router.route('/getWatchHistory').get(verifyJwt,getWatchHistory);
 router.route('/logout').post(verifyJwt,logoutUser)
 
 export default router;
